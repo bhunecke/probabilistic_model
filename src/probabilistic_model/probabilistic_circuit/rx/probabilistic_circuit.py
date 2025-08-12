Add JPT pruning-growing demo and fix likelihood calculation for pruning

src/probabilistic_model/probabilistic_circuit/rx/probabilistic_circuit.py
@@ -1442,24 +1442,26 @@ class ProbabilisticCircuit(ProbabilisticModel, SubclassJSONSerializer):
         :param dataset: The input dataset.
         """
         edge_flows = defaultdict(float)
+        variable_map = self.variable_to_index_map
 
-        for x in dataset:
+        for x in tqdm.tqdm(dataset, desc="Computing circuit flows"):
             # Ensure x is 2D (reshape single sample to batch of size 1)
             if x.ndim == 1:
                 x = x.reshape(1, -1)
                 
             # Forward pass
-            node_log_likelihoods = {}
+            node_likelihoods = {}
             for layer in reversed(self.layers):
                 for node in layer:
                     if isinstance(node, LeafUnit):
-                        # TODO: Test correct leaf unit likelihood computation
-                        node_log_likelihoods[node] = node.distribution.log_likelihood(x[:, [self.variable_to_index_map[var] for var in node.variables]])
+                        variable_indices = [variable_map[var] for var in node.variables]
+                        log_likelihood = node.distribution.log_likelihood(x[:, variable_indices])
+                        node_likelihoods[node] = np.exp(log_likelihood[0])
                     elif isinstance(node, ProductUnit):
                         likelihood = 1.0
                         for child in node.subcircuits:
-                            likelihood *= node_log_likelihoods[child]
-                        node_log_likelihoods[node] = likelihood
+                            likelihood *= node_likelihoods[child]
+                        node_likelihoods[node] = likelihood
                     elif isinstance(node, SumUnit):
                         likelihood = 0.0
                         for child in node.subcircuits:
@@ -1467,22 +1469,22 @@ class ProbabilisticCircuit(ProbabilisticModel, SubclassJSONSerializer):
                             weight = self.graph.get_edge_data(node.index, child.index)
                             if weight is None:
                                 weight = 0.0
-                            likelihood += np.exp(weight) * node_log_likelihoods[child]
-                        node_log_likelihoods[node] = likelihood
+                            likelihood += np.exp(weight) * node_likelihoods[child]
+                        node_likelihoods[node] = likelihood
 
             # Backward pass
             node_flows = {self.root: 1.0}
             for layer in self.layers:
                 for node in layer:
-                    if node == self.root:
+                    if node.index == self.root.index:
                         continue
                     node_flows[node] = 0.0
                     for parent in node.parents:
                         if isinstance(parent, SumUnit):
                             node_flows[node] += node_flows[parent]
                         elif isinstance(parent, ProductUnit):
-                            if node_log_likelihoods[parent] > 0:
-                                contribution = node_log_likelihoods[node] / node_log_likelihoods[parent]
+                            if node_likelihoods[parent] > 0:
+                                contribution = node_likelihoods[node] / node_likelihoods[parent]
                                 node_flows[node] += contribution * node_flows[parent]
 
             # Compute edge flows
@@ -1490,12 +1492,12 @@ class ProbabilisticCircuit(ProbabilisticModel, SubclassJSONSerializer):
                 for node in layer:
                     if isinstance(node, SumUnit):
                         for child in node.subcircuits:
-                            if node_log_likelihoods[node] > 0:
+                            if node_likelihoods[node] > 0:
                                 # Get weight from the edge between parent and child
                                 weight = self.graph.get_edge_data(node.index, child.index)
                                 if weight is None:
                                     weight = 0.0
-                                edge_flow = (np.exp(weight) * node_log_likelihoods[child] / node_log_likelihoods[node] * node_flows[node])
+                                edge_flow = (np.exp(weight) * node_likelihoods[child] / node_likelihoods[node] * node_flows[node])
                                 edge_flows[(node, child)] += edge_flow
 
         return edge_flows
@@ -1546,7 +1548,7 @@ class ProbabilisticCircuit(ProbabilisticModel, SubclassJSONSerializer):
             grown_pc.add_node(node_old_copy)
             grown_pc.add_node(node_new)
 
-            if node == original_root:
+            if node.index == original_root.index:
                 grown_pc.add_edge(new_root, node_old_copy, 0.5)
                 grown_pc.add_edge(new_root, node_new, 0.5)
 